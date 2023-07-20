1f7e33ab4782ab8d09e5adcb8e02976211f73798
calfaro <cesarin28am@gmail.com>
2023-07-20 11:00:06 -0500

    comments

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ const app = require('./src/index');
 // Creo mi app como  aws serverless
 const server =  awsServerlessExpress.createServer(app);
 
-// Función  lambda
+// Función  lambda a mis funcione de api rest
 module.exports.handler = (event, context)=>{
   return awsServerlessExpress.proxy(server, event, context);
 }
